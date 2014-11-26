fc3a89193b2c308649ca800cab02923bc6e56006
Roman Davis <fluffyribbit@gmail.com>
2014-11-27 01:02:45 +0800

    Add stuff

---
+++ gosu-tutorial.rb
@@ -0,0 +1,18 @@
+#tutorial-game/gosu-tutorial.rb
+require "rubygems"
+require "gosu"
+
+class GameWindow < Gosu:Window
+	def initialize
+		super 640, 480, false
+		self.caption = "Gosu Tutorial Game"
+	end
+	
+	def update
+	
+	end
+	
+	def draw
+	
+	end
+end
