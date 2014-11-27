8e729078b0c50f40f4f8b4417d5cb530049e134d
Roman Davis <fluffyribbit@gmail.com>
2014-11-28 02:23:07 +0800

    Add left paddle and movement

--- paddle-game.rb
+++ paddle-game.rb
@@ -11,17 +11,35 @@ class GameWindow < Gosu::Window
 		@x = @y = 100
 		@vx = @vy = 5
 		@ball_image = Gosu::Image.new(self, "ping.png", false)
+		@paddle_image = Gosu::Image.new(self, "pong.png", false)
+		@lp_x, @lp_y, @vlp = 20, 270, 0
 	end
 	
 	def draw
 		@ball_image.draw(@x, @y, 1)
+		@paddle_image.draw(@lp_x, @lp_y, 1)
 	end
 	
 	def update
+
+	#moving the ball
 		@x += @vx
 		@y += @vy
 		@vx = -@vx if @x > 780 || @x < 0
 		@vy = -@vy if @y> 580 || @y < 0
+
+	#moving the left paddle	
+		@lp_y = @lp_y + @vlp
+	end
+	
+	def button_down(id)
+		@vlp -= 5 if id == Gosu::KbW
+		@vlp += 5 if id == Gosu::KbZ
+	end
+	
+	def button_up(id)
+		@vlp += 5 if id == Gosu::KbW
+		@vlp -= 5 if id == Gosu::KbZ
 	end
 end
 
