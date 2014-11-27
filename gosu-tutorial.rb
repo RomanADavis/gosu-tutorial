8cd512f8d248db2f5aebb5445e1f8bd8092f3455
Roman Davis <fluffyribbit@gmail.com>
2014-11-28 00:49:22 +0800

    Add ball

--- gosu-tutorial.rb
+++ gosu-tutorial.rb
@@ -2,10 +2,11 @@
 require "rubygems"
 require "gosu"
 
-class GameWindow < Gosu:Window
+class GameWindow < Gosu::Window
 	def initialize
 		super 640, 480, false
 		self.caption = "Gosu Tutorial Game"
+		@bouncing_ball = Ball.new(self)
 	end
 	
 	def update
@@ -13,6 +14,26 @@ class GameWindow < Gosu:Window
 	end
 	
 	def draw
-	
+		@bouncing_ball.draw
 	end
 end
+
+class Ball
+
+	def initialize(window)
+		@image = Gosu::Image.new(window, "ball.png", false)
+		@x = @y = 50
+	end
+	
+	def draw
+		@image.draw(@x, @y, 1)
+	end
+	
+	def move
+
+	end
+	
+end
+
+window = GameWindow.new
+window.show
